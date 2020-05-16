c709edbc37b7cef19e70fe3820a2803c92ee1da3
Cameron Moore <cmrnmr@gmail.com>
2020-05-16 16:43:12 -0400

    created knexfile and edited development

---
+++ knexfile.js
@@ -0,0 +1,56 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './database/recipebook.sqlite3'
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.runn('PRAGMA foreign_keys = ON', done);
+      },
+    },
+    migrations: {
+      directory: "./migrations",
+    },
+    seeds: {
+      directory: "./seeds",
+    },
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
