2c8a25cbf6296c4d10217b0a6fc2b8e5ac3b204a
Cameron Moore <cmrnmr@gmail.com>
2020-05-27 20:16:44 -0400

    fudged up

--- migrations/20200527195626_recipe_ingredients.js
+++ migrations/20200527195626_recipe_ingredients.js
@@ -1,8 +1,23 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("recipe_ingredients", (tbl) => {
+    tbl.integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("recipes")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+    tbl.integer("ingredients_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("ingredients")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("recipe_ingredients")
 };
