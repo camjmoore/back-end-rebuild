198847709391cfa15fa4358624624ce4c395f578
Cameron Moore <cmrnmr@gmail.com>
2020-05-30 10:09:29 -0400

    created knex queries for category models

---
+++ api/categories/model.js
@@ -0,0 +1,25 @@
+const db = require('../../database/dbconfig');
+
+module.exports = {
+  getCategories,
+  addCategory,
+  editCategories,
+  deleteCategory,
+}
+
+const getCategories = () => {
+  return db('categories');
+}
+
+const addCategory = (category) => {
+  return db('categories').insert(category, 'id');
+}
+
+const editCategories = (changes, id) => {
+  return db('categories').where({id}).update(changes);
+}
+
+const deleteCategory = (id) => {
+  return db('categories').where({ id }).del();
+}
+
