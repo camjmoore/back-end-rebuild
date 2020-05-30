a38891b9614437ac93d0659098d3501b8861dc04
Cameron Moore <cmrnmr@gmail.com>
2020-05-30 09:09:28 -0400

    commented places for request routes

--- api/categories/router.js
+++ api/categories/router.js
@@ -1,2 +1,11 @@
 const express = require('express')
-const router = express.Router
+const router = express.Router();
+
+// * GET REQUEST
+
+// * PUT REQUEST
+
+// * POST REQUEST
+
+// * DELETE REQUEST
+
