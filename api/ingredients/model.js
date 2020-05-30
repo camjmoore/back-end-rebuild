2d5d766b132f512e7e556065b9eb6971adcd15e3
Cameron Moore <cmrnmr@gmail.com>
2020-05-30 10:53:47 -0400

    started multitable queries for ingredient models

--- api/ingredients/model.js
+++ api/ingredients/model.js
@@ -1,16 +1,23 @@
 const db = require('../../database/dbconfig');
 
 module.exports = {
+  getRecipeIngredients,
   getIngredients,
   addIngredients,
   editIngredient,
   deleteIngredient,
 }
 
+/**GET ALL INGREDIENTS */
 const getIngredients = () => {
   return db('ingredients');
 }
 
+/**GET INGREDIENTS FOR A RECIPE */
+const getRecipeIngredients = () => {
+  return db('recipe_ingredients')
+}
+ /**DEFINE ANOTHER AVAILABLE INGREDIENT */
 const addIngredients = (ingredient) => {
   return db('ingredients').insert(ingredient);
 }
