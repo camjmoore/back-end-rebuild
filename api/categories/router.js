3126a5692becc7f517d02ed717949777e265e25b
Cameron Moore <cmrnmr@gmail.com>
2020-06-05 16:49:20 -0400

    renamed authrouter file to router and built out categories endpoints

--- api/categories/router.js
+++ api/categories/router.js
@@ -5,11 +5,54 @@ const Categories = require('./model')
 const router = express.Router();
 
 /** GET REQUEST */
-
-/** PUT REQUEST */
+router.get('/', (req, res) => {
+  Categories.getCategories()
+  .then(cats => {
+    res.status(200).json(cats);
+  })
+  .catch(error => {
+    res.status(400).json({ error: "The categories could not be retrieved" })
+  })
+})
 
 /** POST REQUEST */
+router.post('/', (req, res) => {
+  const newCat = req.body
+
+  Categories.addCategory(newCat)
+    .then(cat => {
+      res.status(201).json({ success: "category created", id: cat[0], ...newCat })
+    })
+    .catch(error => {
+      res.status(400).json({ error: "The category could not be created" })
+    })
+})
+
+/** PUT REQUEST */
+router.put('/:id', (req, res) => {
+  const changes = req.body;
+  const id = req.params.id;
+
+  Categories.editCategories(changes, id)
+    .then(cat => {
+      res.status(200).json({ success: "Category was succcessfully updatede", ...changes })
+    })
+    .catch(error => {
+      res.status(400).json({ error: "the changes could not be saved"})
+    })
+})
 
 /** DELETE REQUEST */
+router.delete('/:id', (req, res) => {
+  const id = req.params.id;
+
+  Categories.deleteCategory(id)
+    .then(del => {
+      res.status(200).json({ success: "Category was successfully deleted" })
+    })
+    .catch(err => {
+      res.status(400).json({ error: "The category could not be deleted" })
+    })
+})
 
 module.exports = router
--- api/server.js
+++ api/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const server = express();
 
 /** ROUTERS */
-const authRouter = require('./auth/authRouter.js')
+const authRouter = require('./auth/router')
 const recipesRouter = require('./recipes/router')
 const categoriesRouter = require('./categories/router')
 const ingredientsRouter = require('./ingredients/router')
@@ -19,7 +19,7 @@ server.use(cors());
 
 /** ROUTES */
 server.use('/api/auth', authRouter)
-// server.use('/api/recipes', recipesRouter)
+server.use('/api/recipes', recipesRouter)
 // server.use('/api/categories', categoriesRouter)
 // server.use('/api/ingredients', ingredientsRouter)
 // server.use('/api/instructions', instructionsRouter)
