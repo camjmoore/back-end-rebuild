f357e91846ca1aa34aa5da942de0e40c064e7afe
Cameron Moore <cmrnmr@gmail.com>
2020-06-08 14:29:44 -0400

    created cleanup seed table

---
+++ seeds/00_cleanup.js
@@ -0,0 +1,12 @@
+
+exports.seed = async function(knex) {
+  // Deletes ALL existing entries
+  await knex('recipe_categorizations').truncate()
+  await knex('recipe_ingredients').truncate()
+  await knex('recipe_instructions').truncate()
+  await knex('recipes').truncate()
+  await knex('categories').truncate()
+  await knex('ingredients').truncate()
+  await knex('instructions').truncate()
+  await knex('users').truncate()
+};
