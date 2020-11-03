570f1efea3b8c028adfd9ff45d9e24975f97e050
Cameron Moore <cmrnmr@gmail.com>
2020-11-03 18:43:32 -0500

    added corresponding ingred and instruc for first 5 recipes

--- seeds/08_recipe_categorizations.js
+++ seeds/08_recipe_categorizations.js
@@ -3,6 +3,10 @@ exports.seed = async function(knex) {
 // Inserts seed entries
   await knex('recipe_categorizations').insert([
     {recipe_id: 1, category_id: 1},
-    {recipe_id: 1, category_id: 2}
+    {recipe_id: 2, category_id: 1},
+    {recipe_id: 3, category_id: 1},
+    {recipe_id: 4, category_id: 1},
+    {recipe_id: 5, category_id: 1},
+
   ]);
 };
