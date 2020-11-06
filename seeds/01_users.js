dd9a20cc47c5ad32e80bab26c20a99cb7e5bec13
Cameron Moore <cmrnmr@gmail.com>
2020-11-06 14:50:47 -0500

    corrected incorrect password values

--- seeds/01_users.js
+++ seeds/01_users.js
@@ -4,8 +4,8 @@ exports.seed = async function(knex) {
   await knex('users').insert([
     {id: 1, user_name: 'Hannibal', password: '$2a$08$S9QmhmSX5rKmfv9r1FgRWOScjQN1y2LKvEYjcXwYr8ibc4EbEExCi'},
     {id: 2, user_name: 'Lilly', password: '$2a$08$vcl.rzvSLXoX1KltpBO8Pec5ItLue1d5NkzamNsU6vxB7hsBNRg0e'},
-    {id: 3, user_name: 'Lucy', password: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Ikx1Y3kiLCJpYXQiOjE2MDQ2OTAyNjcsImV4cCI6MTYwNDc3NjY2N30.cc534n174fmEyNVNR4zyf2Y5YukucE9EYYD1ofI5-lk'},
-    {id: 4, user_name: 'Roxy', password: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IlJveHkiLCJpYXQiOjE2MDQ2OTAzMDcsImV4cCI6MTYwNDc3NjcwN30.rDp4vk3-9VK3N3zp74_YRm3evC7fHpI4RyqW_wR2zjM'},
-    {id: 5, user_name: 'Charity', password: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkNoYXJpdHkiLCJpYXQiOjE2MDQ2OTAzNDIsImV4cCI6MTYwNDc3Njc0Mn0.OmtB56cn8ONHhKkEFLAO1LeA-lA6EKJy4_UT0vVhOWE'}
+    {id: 3, user_name: 'Lucy', password: '$2a$08$tQCrsa6qyY3aMfjD8.cvnebUomZTjcFbh3t9B7JN8YNbT7J5q5CFa'},
+    {id: 4, user_name: 'Roxy', password: '$2a$08$xjH9pa9O8UVWh/aYlkNZzOjtzU/N.o0EhQqIjy6vsw8OdIPHy4ONG'},
+    {id: 5, user_name: 'Charity', password: '$2a$08$MDO5pxPik/mPuit6BFA5tu3RMgB81BxBFYBxToI0V7Op61pyaIjp2'}
   ]);
 };
